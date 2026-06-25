839b6da54a8612b72a160730ad16dd02fe984d15
Danielle <daniellecaricati1@gmail.com>
2026-06-25 12:37:45 -0300

    Implementação de Rotas, paginas de Cadastro e Login

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Cadastro from './pages/cadastro/Cadastro';
 import Home from './pages/home/Home';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
+import Login from './pages/login/Login'
 
 function App(){
   return (
     <>
-      <Navbar />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+            </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </> 
   );
 }
