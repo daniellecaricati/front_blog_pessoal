19ce03c86eed92ce5d104b8d545aa4677eb5ef75
Danielle <daniellecaricati1@gmail.com>
2026-06-30 17:04:16 -0300

    Implementação Crud

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Cadastro from './pages/cadastro/Cadastro';
-import { AuthProvider } from './contexts/AuthContext';
-import Home from './pages/home/Home';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
+import FormTema from './components/tema/formtema/FormTema';
+import ListaTemas from './components/tema/listatemas/ListaTemas';
+import DeletarTema from './components/tema/deletartema/DeletarTema';
+import { AuthProvider } from './contexts/AuthContext';
+import Cadastro from './pages/cadastro/Cadastro';
+import Home from './pages/home/Home';
 import Login from './pages/login/Login'
 
 function App(){
@@ -17,6 +20,10 @@ function App(){
                 <Route path="/" element={<Login />} />
                 <Route path="/home" element={<Home />} />
                 <Route path="/cadastro" element={<Cadastro />} />
+                <Route path="/temas" element={<ListaTemas />} />
+                <Route path="/cadastrartema" element={<FormTema />} />
+                <Route path="/editartema/:id" element={<FormTema />} />
+                <Route path="/deletartema/:id" element={<DeletarTema />} />
               </Routes>
          </div>
           <Footer />
